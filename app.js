9503b347a91566727bb15dd68e04175f8b32311a
Jeong Jinwoo <jwjeong96@gmail.com>
2022-03-28 20:54:22 +0900

    fix: remove environment variables

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require("./routes/index");
+var users = require("./routes/users");
 
 var app = express();
 
-// TODO: 배포전 삭제 임시변수
-var aws_access_key_id = ***REMOVED***;
-var aws_secret_access_key=***REMOVED***;
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/board1', require('./routes/board1'));
-app.use('/board2', require('./routes/board2'));
+app.use("/", index);
+app.use("/users", users);
+app.use("/board1", require("./routes/board1"));
+app.use("/board2", require("./routes/board2"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
